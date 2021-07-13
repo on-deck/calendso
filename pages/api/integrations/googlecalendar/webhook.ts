d5f890dc64c509708364149f999e8b7efae8bf68
Egor Zaitsev <heuels@gmail.com>
2021-07-13 18:00:25 +0300

    (schedule) send 200 OK in response to webhooks

--- pages/api/integrations/googlecalendar/webhook.ts
+++ pages/api/integrations/googlecalendar/webhook.ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.error("req.query", req.query);
   console.error("req.body", req.body);
-  return null;
+  return res.status(200).end();
 }
