791f64f1878c94c83505e2b8fad0a7a588fcc898
Egor Zaitsev <heuels@gmail.com>
2021-07-14 17:21:47 +0300

    (schedule) add a prefix to mixpanel events

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -3,11 +3,11 @@ import Analytics from "analytics-node";
 export const analytics = new Analytics(process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY);
 
 export enum AnalyticsTrackingEvent {
-    BookingCancelled = "bookingCancelled",
-    BookingConfirmed = "bookingConfirmed",
-    LoginFirstTime = "loginFirstTime",
-    LoginSuccess = "loginSuccess",
-    LoginNotAllowed = "loginNotAllowed",
-    DateSelected = "dateSelected",
-    TimeSelected = "timeSelected",
+    BookingCancelled = "scheduleBookingCancelled",
+    BookingConfirmed = "scheduleBookingConfirmed",
+    LoginFirstTime = "scheduleLoginFirstTime",
+    LoginSuccess = "scheduleLoginSuccess",
+    LoginNotAllowed = "scheduleLoginNotAllowed",
+    DateSelected = "scheduleDateSelected",
+    TimeSelected = "scheduleTimeSelected",
 }
